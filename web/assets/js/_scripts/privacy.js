07bf97159b94642db2e41f739c3758ae2aaee8a1
Dieter Peirs <github@dieterpeirs.com>
2018-08-22 19:09:44 +0200

    update cookie notice and privacy policy

--- web/assets/js/_scripts/privacy.js
+++ web/assets/js/_scripts/privacy.js
@@ -2,17 +2,24 @@ $(document).ready(function(){
   var privacy = $('.js-privacy');
   var privacyButton = $('.js-privacy__button');
 
-  function setCookie(c_name,value,exdays) {
-    var exdate=new Date();
-    exdate.setDate(exdate.getDate() + exdays);
-    var c_value=escape(value) + ((exdays==null) ? "" : ("; expires="+exdate.toUTCString()));
-    document.cookie=c_name + "=" + c_value;
-  }
+  function writeCookie(key, value, days) {
+    var date = new Date();
+
+    // Default at 365 days.
+    days = days || 365;
+
+    // Get unix milliseconds at current time plus number of days
+    date.setTime(+ date + (days * 86400000)); //24 * 60 * 60 * 1000
+
+    window.document.cookie = key + "=" + value + "; expires=" + date.toGMTString() + "; path=/";
+
+    return value;
+  };
 
   function setPrivacy(e) {
     privacyButton.blur();
     privacy.toggleClass('u-hidden');
-    setCookie('PrivacyNotice','Nerdlab cookie notice');
+    writeCookie('PrivacyNotice','NerdlabCookieNotice');
   }
 
   privacyButton.on('click', setPrivacy);
