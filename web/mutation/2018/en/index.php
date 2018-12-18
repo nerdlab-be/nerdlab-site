326a7193b117bde895c7630385ef2550329c15f3
Dieter Peirs <github@dieterpeirs.com>
2018-12-18 15:30:44 +0100

    add blog setup, archive old mutation site

---
+++ web/mutation/2018/en/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Craft web bootstrap file
+ */
+
+// Multisite
+define('CRAFT_SITE','mutationen2018');
+
+// Set path constants
+define('CRAFT_VENDOR_PATH', '/vendor');
+$root = dirname('../../../../craft/vendor');
+define('CRAFT_BASE_PATH', dirname('../../../../craft/vendor'));
+
+// Load Composer's autoloader
+require_once $root.'/vendor/autoload.php';
+
+// Load dotenv?
+if (file_exists($root.'/.env')) {
+  $dotenv = new Dotenv\Dotenv($root);
+  $dotenv->load();
+}
+
+// Load and run Craft
+$app = require $root.'/vendor/craftcms/cms/bootstrap/web.php';
+$app->run();
